093e9a1a81fe78f6383fa04c9f4cc0548a07e1ad
sweir27 <sweir27@gmail.com>
2014-09-14 19:09:20 -0400

    fixing nil issues

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,6 @@ class User < ActiveRecord::Base
   before_save { self.slug = create_slug }
   before_create :create_remember_token
   validates :name, presence: true, length: { maximum: 50 }
-  validates :biography, presence: true
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence:   true,
                     format:     { with: VALID_EMAIL_REGEX },
