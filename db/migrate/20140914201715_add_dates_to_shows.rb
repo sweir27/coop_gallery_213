90a681ef3395816b23cd06356a5d8b377a3893f8
sweir27 <sweir27@gmail.com>
2014-09-14 18:42:18 -0400

    again fixing

--- db/migrate/20140914201715_add_dates_to_shows.rb
+++ db/migrate/20140914201715_add_dates_to_shows.rb
@@ -1,6 +1,6 @@
 class AddDatesToShows < ActiveRecord::Migration
   def change
-    add_column :shows, :start_date, :datetime
-    add_column :shows, :end_date, :datetime
+    add_column :links, :start_date, :datetime
+    add_column :links, :end_date, :datetime
   end
 end
