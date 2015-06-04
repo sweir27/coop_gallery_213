77fa714924945563c6a17eaa4d4731f2b79496bd
Ken Weir <kaweir@stny.rr.com>
2015-06-04 19:04:51 -0400

    restored development.rb to non-windows version

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -36,6 +36,6 @@ CoopGallery213::Application.configure do
   # config.action_view.raise_on_missing_translations = true
 
   # config/environments/production.rb
-
-  Paperclip.options[:command_path] = "C:/Program Files/ImageMagick-6.9.1-Q16"
+  Paperclip.options[:command_path] = "/usr/local/bin/"
+  # Paperclip.options[:command_path] = "C:/Program Files/ImageMagick-6.9.1-Q16"
 end
