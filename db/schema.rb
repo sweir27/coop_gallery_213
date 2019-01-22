dd2870a05c03bbd2018ef4bbb0f4ece88fa7dfe9
Sarah Weir <sweir27@gmail.com>
2019-01-21 19:50:27 -0500

    remove unnecessary migrations

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160415054637) do
+ActiveRecord::Schema.define(version: 20150425212344) do
 
   create_table "announcements", force: true do |t|
     t.string   "title"
