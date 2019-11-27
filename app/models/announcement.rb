d50f7e8de295625bfdeadcb0743a0ad755151dc4
Sarah Weir <sweir27@gmail.com>
2019-11-27 17:27:27 -0500

    Announcement images

--- app/models/announcement.rb
+++ app/models/announcement.rb
@@ -4,10 +4,7 @@ class Announcement < ActiveRecord::Base
 
   validates_presence_of :pdf, :unless => lambda { self.pdf_title.blank? }
 
-  has_attached_file :announcement_image,
-                    :styles =>
-                      { :thumb => "150x150#", :largethumb => "250x250#" }
-  validates_attachment_content_type :announcement_image, :content_type => /\Aimage\/.*\Z/
+  has_one_attached :announcement_image
 
   has_attached_file :pdf
   validates_attachment_content_type :pdf,
